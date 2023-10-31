f4093a993776380af2d65fc42bd5b4b65e2837e8
alinaswe45@gmail.com <85223818+Alinaswe3@users.noreply.github.com>
2023-10-31 15:35:18 +0200

    Made the features responsive

--- src/lib/sections/Feature.svelte
+++ src/lib/sections/Feature.svelte
@@ -2,14 +2,16 @@
     export let iconUrl: string;
 </script>
 
-<div class="flex flex-col gap-[2.4rem]">
-    <figure class="mb-[2.2rem] w-16 h-16 rounded-[50%] overflow-hidden">
+<div class="flex flex-col gap-[4.6rem] tablet:gap-[4rem] tablet:flex-row">
+    <figure class="w-[6.4rem] h-[6.4rem] rounded-[50%] overflow-hidden">
         <img alt="Feature icon" class="w-full h-full" src={iconUrl}/>
     </figure>
-    <h3 class="heading text-h-md tracking-[-1.2px]">
-        <slot name="header">No header was provided</slot>
-    </h3>
-    <p class="text-b-md text-dark-electric-blue leading-body">
-        <slot name="body">No content was provided</slot>
-    </p>
+    <div class="flex flex-col gap-[2.4rem]">
+        <h3 class="heading text-h-md tracking-[-1.2px]">
+            <slot name="header">No header was provided</slot>
+        </h3>
+        <p class="text-b-md text-dark-electric-blue leading-body">
+            <slot name="body">No content was provided</slot>
+        </p>
+    </div>
 </div>
